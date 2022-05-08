d058eaf1e7fd8d29207e8ccc2a04060bdc1e7645
ahmednasser74 <ahnasser213@gmail.com>
2022-05-08 07:02:36 +0200

    finish transcript screen

--- lib/features/student_data/presentation/controller/transcript_controller.dart
+++ lib/features/student_data/presentation/controller/transcript_controller.dart
@@ -1,3 +1,12 @@
+import 'package:boilerplate/core/utils/pref_util.dart';
 import 'package:get/get.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
-class TranscriptController extends GetxController {}
+class TranscriptController extends GetxController {
+  final PdfViewerController pdfViewerController = PdfViewerController();
+  final pdfUrl = 'https://sisv2.esu.ac.ae/api/transcript';
+  final headers = {
+    'Accept': 'application/json',
+    'Authorization': 'Bearer ${SharedPrefs.instance.getToken()}',
+  };
+}
--- lib/features/student_data/presentation/screen/transcript_screen.dart
+++ lib/features/student_data/presentation/screen/transcript_screen.dart
@@ -1,9 +1,8 @@
 import 'package:boilerplate/core/localization/localization_keys.dart';
 import 'package:boilerplate/features/student_data/presentation/controller/transcript_controller.dart';
-import 'package:boilerplate/features/student_data/presentation/widgets/transcript_item_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class TranscriptScreen extends GetView<TranscriptController> {
   const TranscriptScreen({Key? key}) : super(key: key);
@@ -12,19 +11,29 @@ class TranscriptScreen extends GetView<TranscriptController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(LocalizationKeys.transcript.tr)),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.separated(
-              itemCount: 10,
-              padding: EdgeInsets.symmetric(horizontal: 18.w, vertical: 6.h),
-              separatorBuilder: (context, index) => SizedBox(height: 12.h),
-              itemBuilder: (context, index) =>
-                  TranscriptItemWidget(index: index),
-            ),
-          ),
-        ],
+      // floatingActionButton: FloatingActionButton.extended(
+      //   onPressed: () {},
+      //   label: Text('Download'),
+      //   icon: const Icon(Icons.file_download),
+      // ),
+      body: SfPdfViewer.network(
+        controller.pdfUrl,
+        controller: controller.pdfViewerController,
+        headers: controller.headers,
       ),
+      // body: Column(
+      //   children: [
+      //     Expanded(
+      //       child: ListView.separated(
+      //         itemCount: 10,
+      //         padding: EdgeInsets.symmetric(horizontal: 18.w, vertical: 6.h),
+      //         separatorBuilder: (context, index) => SizedBox(height: 12.h),
+      //         itemBuilder: (context, index) =>
+      //             TranscriptItemWidget(index: index),
+      //       ),
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
